1b6913e9d70e0a30604a9548dab61592006fb66a
QformX <101177599+QformX@users.noreply.github.com>
2023-06-23 18:28:11 +0600

    Add files via upload

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,6 +4,28 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        throw new NotImplementedException();
+        if (a == 0)
+        {
+            throw new ArgumentException();
+        }
+
+        if (a == double.NaN || b == double.NaN || a == double.MaxValue || b == double.MaxValue)
+        {
+            throw new ArgumentException();
+        }
+
+        double d = b * b - 4 * a * c;
+
+        if (d < 0) { return new double[0]; }
+        
+        if (d == 0)
+        {
+            double x = -b / 2 * a;
+            return new double[] { x, x };
+        }
+
+        double x1 = -(b + Math.Sign(b) * Math.Pow(d, 0.5) / 2 * a);
+        double x2 = c / x1;
+        return new double[] { x1, x2 };
     }
 }
