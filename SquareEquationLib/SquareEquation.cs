237c80840d629408b2b2ad8591b3f67d0fea4964
QformX <101177599+QformX@users.noreply.github.com>
2023-06-23 20:11:24 +0600

    Add files via upload

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,22 +4,22 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        if (Math.Abs(a) < double.Epsilon || new double[] {a, b, c}.Any(double.IsNaN) || new double[] { a, b, c }.Any(double.IsInfinity))
+        if (Math.Abs(a) < double.Epsilon || (new double[] {a, b, c}).Any(double.IsNaN) || (new double[] { a, b, c }).Any(double.IsInfinity))
         {
-            throw new System.ArgumentException();
+            throw new ArgumentException();
         }
 
-        double d = b * b - 4 * a * c;
+        double d = Math.Pow(b, 2.0) - 4 * c;
 
         if (d < 0) { return Array.Empty<double>(); }
         
         if (Math.Abs(d) < double.Epsilon)
         {
-            double x = -b / 2 * a;
+            double x = -b / (2 * a);
             return new double[] { x };
         }
 
-        double x1 = -(b + Math.Sign(b) * Math.Sqrt(d) / 2);
+        double x1 = -((b + Math.Sign(b) * Math.Sqrt(d)) / 2);
         double x2 = c / x1;
         return new double[] { x1, x2 };
     }
