4cc7157f1a9e538b6f2a4fe4760cef29e08a2083
QformX <101177599+QformX@users.noreply.github.com>
2023-06-23 21:35:59 +0600

    Add files via upload

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,22 +4,33 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        if (Math.Abs(a) < double.Epsilon || (Math.Abs(a) < double.Epsilon && Math.Abs(b) < double.Epsilon && Math.Abs(c) < double.Epsilon) || (new double[] {a, b, c}).Any(double.IsNaN) || (new double[] { a, b, c }).Any(double.IsInfinity))
+        if (Math.Abs(a) < double.Epsilon || (Math.Abs(a) < double.Epsilon && Math.Abs(b) < double.Epsilon && Math.Abs(c) < double.Epsilon) || (new double[] { a, b, c }).Any(double.IsNaN) || (new double[] { a, b, c }).Any(double.IsInfinity))
         {
             throw new ArgumentException();
         }
 
-        double d = Math.Pow(b, 2.0) - (4 * a * c);
+        double d = 0.0;
+
+        if (Math.Abs(c) < double.Epsilon)
+        {
+            double x = -b / a;
+            return new double[] { x, 0 };
+        }
+        else
+        {
+            d = Math.Pow(b, 2.0) - (4 * a * c);
+        }
+
 
         if (d < 0.0) { return Array.Empty<double>(); }
-        
+
         if (Math.Abs(d) < double.Epsilon)
         {
             double x = -b / (2 * a);
             return new double[] { x };
         }
 
-        double x1 = -((b + Math.Sign(b) * Math.Sqrt(d)) / (2*a));
+        double x1 = -((b + Math.Sign(b) * Math.Sqrt(d)) / (2 * a));
         double x2 = c / (x1 * a);
         return new double[] { x1, x2 };
     }
